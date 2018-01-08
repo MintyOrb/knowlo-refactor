2e76a733c8dd42033cb05198dafba6f7c4f9f66e
Tim Borny <bornytm@gmail.com>
2018-01-07 18:58:58 -0800

    added server; added view imports to router;

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,30 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-// need to import other comps
+import explore from '@/views/explore'
+import getInvolved from '@/views/getInvolved'
+import landing from '@/views/landing'
+import legal from '@/views/legal'
+import member from '@/views/member'
+import principals from '@/views/principals'
+import resource from '@/views/resource'
+import status from '@/views/status'
+import token from '@/views/token'
+import newResource from '@/components/newResource'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
-    {  name: "explore", path: '/', component: explore,
+    // { path: '/m/:uid', component: member, name: 'member' },
+    // { path: '/r/:uid', component: resource, name: 'resource' },
+    { path: '/', component: explore,  name: "explore",
         children: [
-          { path: '/m/:uid', component: memberPage, name: 'memberPage' },
-          { path: '/r/:uid', component: resourceComp, name: 'resourceSub' },
-          { path: '/t/:name/:uid?', component: termComp, name: 'setSub' },
-          { path: '/addResource', component: addResource, name: 'addResource' },
-          { path: '/addTerm/:translation/:termID?', component: addTerm, name: 'addTerm' },
+          { path: '/m/:uid', component: member, name: 'member' },
+          { path: '/r/:uid', component: resource, name: 'resource' },
+          { path: '/t/:name/:uid?', component: token, name: 'token' },
+          { path: '/addResource', component: addResource, name: 'addResource' }
         ]
     },
-    {  path: "/trending", component: trending },
     {  path: "/status", component: status },
     {  path: "/about", component: landing },
     {  path: "/principals", component: principals },
